5258488656744a1e81a073c2dfffdf96de5dd932
Alexis Tessier <alexis.tessier@designdo.fr>
2016-02-07 03:57:30 +0100

    add reflection

--- experiments/cube-raytracing/build.glsl
+++ experiments/cube-raytracing/build.glsl
@@ -462,33 +462,31 @@ void rasterize(inout vec3 vertices[CubeVerticesCount], Camera cam){
 
 const lowp vec4 backgroundColor = COLOR_BLACK;
 
-vec4 castRay(vec3 origin, vec3 direction, Triangle triangles[NUMBER_OF_TRIANGLES]){
+vec4 castRay(highp vec3 origin, highp vec3 direction, Triangle triangles[NUMBER_OF_TRIANGLES]){
 	vec4 color = backgroundColor;
 
 	float t, u, v;
 	Triangle closestTriangle;
-	bool intersect = true;
 	
-
-	float depthReflexion = 1.0;
+	float mixFactor = 1.0;
 	for(int depth = 0;depth < RAYCAST_MAXIMUM_DEPTH;depth++){
-		intersect = firstHitTriangle(closestTriangle,
+		if(firstHitTriangle(closestTriangle,
 			origin,
 			direction,
 			triangles, 
 			t, u, v
-		);
-
-		if(intersect){
+		)){
 			vec3 triangleNormal = normal(closestTriangle);
-			float reflection = dot(direction, triangleNormal);
-			color += (closestTriangle.material.color*abs(reflection)*depthReflexion);
+			
+			color = mix(color, closestTriangle.material.color, mixFactor);
 
-			depthReflexion *= closestTriangle.material.reflection;
+			if(closestTriangle.material.reflection > 0.0){
+				mixFactor *= closestTriangle.material.reflection;
 
-			origin = direction*t;
-			vec3 ax = cross(direction, triangleNormal);
-			rotate(direction, ax, radians(180.0-2.0*degrees(reflection)));
+				float incidentAngle = acos(dot(-direction, triangleNormal));
+				origin += (direction*t);
+				rotate(direction, -cross(direction, triangleNormal), 2.0*incidentAngle);
+			}
 		}else{
 			break;
 		}
@@ -508,31 +506,30 @@ vec4 trace(vec2 fragCoord, Camera cam, Triangle triangles[NUMBER_OF_TRIANGLES]){
 void mainImage(out vec4 fragColor, in vec2 fragCoord){
 	Triangle triangles[NUMBER_OF_TRIANGLES];
 
-	float rot = mod(25.0*iGlobalTime, 360.0);
-
 	Triangle greenTriangle = Triangle(mat3(
 		0.5, 0.5, 0.0,
 		0.0, 0.0, 0.0,
 		1.0, 0.0, 0.0),
-		Material(COLOR_ORANGE, 0.5)
+		Material(COLOR_GREEN, 0.8)
 	);
-	rotate(greenTriangle, AXIS_Y, rot);
+	//rotate(greenTriangle, AXIS_Y, radians());
 
 
-	Triangle redTriangle = Triangle(mat3(
-		1.5, 0.5, 0.0,
-		0.0, 1.0, 0.0,
-		0.8, 0.5, 0.0),
-		Material(COLOR_PURPLE, 0.8)
+	Triangle orangeTriangle = Triangle(mat3(
+		0.5, 0.5, 0.0,
+		0.0, 0.0, 0.0,
+		1.0, 0.0, 0.0),
+		Material(COLOR_ORANGE, 0.0)
 	);
+	translate(orangeTriangle, vec3(0.5, 0.0, 1.0));
 
 	triangles[0]=greenTriangle;
-	triangles[1]=redTriangle;
+	triangles[1]=orangeTriangle;
 
 	/*--------------*/
 	Camera cam = initCamera(
-		/*position*/greenTriangle.vertices[0]+vec3(0.0, 0.0, 4.2),
-		/*target*/greenTriangle.vertices[0],
+		/*position*/vec3(-0.13, 0.12, 1.4),
+		/*target*/POINT_ZERO,
 		/*fov*/80.0,
 		/*roll*/0.0,
 		/*resolution*/iResolution.xy
